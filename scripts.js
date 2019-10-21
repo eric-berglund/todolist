5ca3b6177c03bb6cfc5992f0da5c2217d3dec125
Eric Berglund <43348893+ejberglund@users.noreply.github.com>
2019-10-21 15:21:08 -0500

    set width of form to 75% and added script for day vs days

--- scripts.js
+++ scripts.js
@@ -1,22 +1,33 @@
+/* eslint-disable eqeqeq */
 // eslint-disable-next-line no-unused-vars
 function myAlert(task, days) {
   const wrapper = document.createElement('div');
+
+  let dayOrDays = '';
+  if (days == 1) {
+    dayOrDays = 'day';
+  } else {
+    dayOrDays = 'days';
+  }
 
   // using bootstrap naming convention, determine the color of the alert based on how
   // many days the taks needs to be completed by
   let alertColor = '';
   if (days < 3) {
-    alertColor = 'alert-danger';
+    alertColor = 'alert-danger'; // red alert box
   } else if (days < 7) {
-    alertColor = 'alert-warning';
+    alertColor = 'alert-warning'; // yellow alert box
   } else {
-    alertColor = 'alert-secondary';
+    alertColor = 'alert-secondary'; // grey alert box
   }
+
+  // create the HTML for the alert using form input data
   wrapper.innerHTML = `<div class="alert ${alertColor} alert-dismissible fade show" role="alert">
-    ${task}</br>${days}
+    ${task}</br>${days} ${dayOrDays}
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
     </div>`;
+  // add the alert to the page
   document.getElementById('alerts').appendChild(wrapper);
 }
