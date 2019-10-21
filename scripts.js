9c4b1696eb3b65e96a206d75a658d3230ba6297f
Eric Berglund <43348893+ejberglund@users.noreply.github.com>
2019-10-21 15:00:19 -0500

    core of the project is completed

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
-function newTask() {
-  const task = (document.getElementById('task').value);
-  const days = (document.getElementById('days').value);
+// eslint-disable-next-line no-unused-vars
+function myAlert(task, days) {
+  const wrapper = document.createElement('div');
 
-  alert(task+days);
+  // using bootstrap naming convention, determine the color of the alert based on how
+  // many days the taks needs to be completed by
+  let alertColor = '';
+  if (days < 3) {
+    alertColor = 'alert-danger';
+  } else if (days < 7) {
+    alertColor = 'alert-warning';
+  } else {
+    alertColor = 'alert-secondary';
+  }
+  wrapper.innerHTML = `<div class="alert ${alertColor} alert-dismissible fade show" role="alert">
+    ${task}</br>${days}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+    </div>`;
+  document.getElementById('alerts').appendChild(wrapper);
 }
