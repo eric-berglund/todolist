47e44b7d5ecc88fb2d90010d568c676d122367d2
Eric Berglund <43348893+ejberglund@users.noreply.github.com>
2019-10-27 17:24:04 -0500

    added code to calculate days from a given date

--- scripts.js
+++ scripts.js
@@ -5,19 +5,21 @@ function myAlert(task, date) {
 
   const currentDate = new Date();
   const goalDate = new Date(date);
+  // find the difference, in milliseconds, between current time and goal time
   const timeDifference = goalDate.getTime() - currentDate.getTime();
-  const days = timeDifference / (100 * 3600 * 24);
+  // convert milliseconds to days
+  const days = Math.floor(timeDifference / (1000 * 3600 * 24));
 
   // functionality to display 'day' for 1 day and 'days' for values > 1
-  let dayOrDays = '';
+  let dayOrDaysLabel = '';
   if (days == 1) {
-    dayOrDays = 'day';
+    dayOrDaysLabel = 'day';
   } else {
-    dayOrDays = 'days';
+    dayOrDaysLabel = 'days';
   }
 
   // using bootstrap naming convention, determine the color of the alert based on how
-  // many days the taks needs to be completed by
+  // many days are remaining for the task to be completed
   let alertColor = '';
   if (days < 3) {
     alertColor = 'alert-danger'; // red alert box
@@ -29,7 +31,7 @@ function myAlert(task, date) {
 
   // create the HTML for the alert using form input data
   wrapper.innerHTML = `<div class="alert ${alertColor} alert-dismissible fade show" role="alert">
-    ${task}</br>${days} ${dayOrDays}
+    ${task}</br>${days} ${dayOrDaysLabel}
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
